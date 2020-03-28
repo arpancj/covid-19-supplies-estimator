06c49f0b25283db5f835c5c9d3303235714ae6ee
Chetan M <chetan.m@surya-soft.com>
2020-03-28 17:10:44 +0530

    Show table data

--- src/App.vue
+++ src/App.vue
@@ -8,40 +8,91 @@
     </div>
     <div id="result">
       <div id="sub-title">Estimated Amount</div>
-      <div id="description">Total Amount ( Number of families * Monthly package amount) = INR {{totalAmount}} for {{numberOfFamilies}} families</div>
+      <div id="description">Total Amount ( Number of families * Monthly package amount) = <b>{{totalAmount}}</b> for {{numberOfFamilies}} families</div>
       <div id="sub-description">This is an estimated amount and not the actual, actual price may slightly vary.</div>
       <div id="table">
         <div id="table-description">Monthly Consumption Expenses of Essential Commodities (For a family of 4-5 members)</div>
-        <table class="table table-bordered">
-          <thead>
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">Items</th>
-              <th scope="col">Quantity</th>
-              <th scope="col">Unit of Measurement</th>
-            </tr>
-          </thead>
-          <tbody>
-          </tbody>
-        </table>
+        <b-table table-bordered striped hover :items="tableItems"></b-table>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+// Canonical data - modify this to make changes to the webpage.
+const _items = [
+    // Item, Quantity, Unit of Measurement, Price Per Unit
+    ["Rice", 12, "KG", 65],
+    ["Ragi", 5, "KG", 25],
+    ["Wheat Flour", 5, "KG", 50],
+    ["Oil", 5, "Liter", 100],
+    ["Sugar", 3, "KG", 55],
+    ["Salt", 1, "KG", 18],
+    ["Onion", 4, "KG", 40],
+    ["Potatoes", 4, "KG", 30],
+    ["Toor Dal", 2, "KG", 60],
+    ["Urad Dal", 1, "KG", 120],
+];
+
+function _formatINR(number) {
+    const roundedUp = Math.ceil(number / 10) * 10;
+    const formatted = new Intl.NumberFormat(
+        'en-IN', 
+        {
+            style: 'currency', 
+            currency: 'INR'
+        }
+    ).format(roundedUp);
+    return formatted.replace(".00", "");
+}
+
+function calcuatePackageCost(numFamilies) {
+    var cost = 0;
+    _items.forEach(item => {
+        cost += (item[1] * item[3]);
+    });
+    cost += (cost * 0.05); // Add 5% transportation cost
+    const totalCost = cost * numFamilies;
+    return _formatINR(totalCost);
+}
+
+function getTableData() {
+    const ret = JSON.parse(JSON.stringify(_items)); // Deep copy
+    ret.push(["Contingency & travels (5%)", null, null, null]);
+    return ret;
+}
+
 export default {
   data () {
     return {
       text: '',
-      totalAmount: 54000,
-      numberOfFamilies: 0
+      totalAmount: '₹ 0',
+      numberOfFamilies: 0,
+      tableData: '',
+      tableItems: ''
     }
   },
   methods: {
     showResult: function () {
       this.numberOfFamilies = this.text
+      if (this.numberOfFamilies === '') {
+        this.numberOfFamilies = '0'
+      }
+      this.totalAmount = calcuatePackageCost(this.numberOfFamilies)
+      var tableData = getTableData
+    },
+    renderTableData() {
+      var rows = []
+      var value
+      for (value in getTableData()) {
+        rows.push({Items: value[0], Quantity: value[1], Unit_Of_Measurement: value[2]})
+      }
+      return rows
+      
     }
+  },
+  created() {
+    this.tableItems = this.renderTableData()
   }
 }
 </script>
